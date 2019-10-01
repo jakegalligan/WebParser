a329d150eea6c0d615efe6e1582af10b7c484772
jakegalligan <galligan1996@gmail.com>
2019-10-01 12:04:43 -0400

    Create hashmap to store the count of each word on the page

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,15 +1,20 @@
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Scanner;
 import org.jsoup.Jsoup;
 
 public class Application {
 	
+	  //create hashmap to store each individual word
+	  public HashMap<Integer, String>   = new HashMap<Integer, String>();
+
 	//this method begins the application after being called from the Main class
 	public void start () throws IOException {
 		//prompt the user to enter the url and store their response within a string variable
 		System.out.println("Enter the url");
 		Scanner scanner = new Scanner(System.in);
 		String url = scanner.nextLine();
+		scanner.close();
 		
 		//take the url and pass it to class method in order to be scraped
 		this.scrapeURLForText(url);		
@@ -18,12 +23,11 @@ public class Application {
 	public void scrapeURLForText (String url) throws IOException {
 		 //take in user inputed url and get text from webpage
 		 String textFromUrl = Jsoup.connect("https://www.quora.com/What-does-throws-IOexception-mean").get().text();
-		 //
-		 String arryOfText[] = textFromUrl.split(" ");
-
-		 System.out.println(textFromUrl);
-
-	}
-	
-	
+		 //store each individual word from the textFromUrl into an array
+		 String arrayOfTextFromUrl[] = textFromUrl.split(" ");
+		 for(String word: arrayOfTextFromUrl) {
+			 System.out.println(word);
+		 }
+		 		 
+	}	
 }
