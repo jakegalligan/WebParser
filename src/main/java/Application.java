b22c31b1a44273a2bee6976e5040d22223e074a4
jakegalligan <galligan1996@gmail.com>
2019-10-01 11:09:44 -0400

    Take in user input for which url the user wants scraped

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
 
 public class Application {
-
+	
+	//this method begins the application after being called from the Main class
+	public void start () {
+		//promp the user to enter the url and store their response within a string variable
+		System.out.println("Enter the url");
+		Scanner scanner = new Scanner(System.in);
+		String url = scanner.nextLine();
+		
+		System.out.println(url);
+	}
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hi");
+		Application application = new Application();
+		application.start();
+		
 
 	}
 
