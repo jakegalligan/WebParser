cd3bda88a7b6a6b7e745a5553f42c65836749214
jakegalligan <galligan1996@gmail.com>
2019-10-01 11:44:41 -0400

    Get text back from url user input and store the text into an array

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,14 +1,29 @@
+import java.io.IOException;
 import java.util.Scanner;
+import org.jsoup.Jsoup;
 
 public class Application {
 	
 	//this method begins the application after being called from the Main class
-	public void start () {
-		//promp the user to enter the url and store their response within a string variable
+	public void start () throws IOException {
+		//prompt the user to enter the url and store their response within a string variable
 		System.out.println("Enter the url");
 		Scanner scanner = new Scanner(System.in);
 		String url = scanner.nextLine();
 		
-		System.out.println(url);
+		//take the url and pass it to class method in order to be scraped
+		this.scrapeURLForText(url);		
 	}
+	
+	public void scrapeURLForText (String url) throws IOException {
+		 //take in user inputed url and get text from webpage
+		 String textFromUrl = Jsoup.connect("https://www.quora.com/What-does-throws-IOexception-mean").get().text();
+		 //
+		 String arryOfText[] = textFromUrl.split(" ");
+
+		 System.out.println(textFromUrl);
+
+	}
+	
+	
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,10 @@
+import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Application application = new Application();
 		application.start();
-		
-
 	}
 
 }
